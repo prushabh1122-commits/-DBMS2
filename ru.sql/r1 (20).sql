bad44b52567ff69caf144b79e50ac414091ddbe6
prushabh1122-commits <prushabh1122@gmail.com>
2026-09-09 15:14:33 +0530

    Update r1 (20).sql

--- ru.sql/r1 (20).sql
+++ ru.sql/r1 (20).sql
@@ -1,7 +1,7 @@
 SET SERVEROUTPUT ON;
 
 DECLARE
-    v_name VARCHAR2(50) := 'Manush Mistry';
+    v_name VARCHAR2(50) := 'rushabh';
     v_dob DATE := TO_DATE('15-05-2007', 'DD-MM-YYYY');
     v_roll NUMBER := 22;
 
