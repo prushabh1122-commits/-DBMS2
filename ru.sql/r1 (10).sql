57ebfa4a39a50a2fc69c8a528110f49942e2e3cc
prushabh1122-commits <prushabh1122@gmail.com>
2026-09-09 15:00:50 +0530

    Update r1 (10).sql

--- ru.sql/r1 (10).sql
+++ ru.sql/r1 (10).sql
@@ -1,25 +1,20 @@
 SET SERVEROUTPUT ON;
-
 DECLARE
-    v_units NUMBER := 100;
-    c_rate CONSTANT NUMBER := 5;
-    c_fixed CONSTANT NUMBER := 70;
-
-    v_energy_charge NUMBER;
-    v_total NUMBER;
-
+v_roll NUMBER := 1;
+v_celsius NUMBER := MOD(v_roll, 20) + 25;
+v_fahrenheit NUMBER;
+v_kelvin NUMBER;
+v_f NUMBER := 98.6;
+v_c2 NUMBER;
 BEGIN
-    -- Calculate electricity charges
-    v_energy_charge := v_units * c_rate;
-    v_total := v_energy_charge + c_fixed;
-
-    -- Print Electricity Bill
-    DBMS_OUTPUT.PUT_LINE('--- TORRENT POWER BILL ---');
-    DBMS_OUTPUT.PUT_LINE('Consumer Name: Manush');
-    DBMS_OUTPUT.PUT_LINE('Units Consumed: ' || v_units);
-    DBMS_OUTPUT.PUT_LINE('Energy Charge: Rs. ' || v_energy_charge);
-    DBMS_OUTPUT.PUT_LINE('Fixed Charge: Rs. ' || c_fixed);
-    DBMS_OUTPUT.PUT_LINE('Total Bill: Rs. ' || v_total);
-
+v_fahrenheit := ROUND(v_celsius * 9 / 5 + 32, 1);
+v_kelvin := v_celsius + 273.15;
+v_c2 := (v_f - 32) * 5 / 9;
+DBMS_OUTPUT.PUT_LINE('Celsius: ' || v_celsius);
+DBMS_OUTPUT.PUT_LINE('Fahrenheit: ' || v_fahrenheit);
+DBMS_OUTPUT.PUT_LINE('Kelvin: ' || v_kelvin);
+DBMS_OUTPUT.PUT_LINE('98.6 F in Celsius: ' || ROUND(v_c2, 2));
+-- C*9/5+32 and C*(9/5)+32 give the same result in Oracle.
+-- Multiplication and division have the same precedence and are evaluated left-to-right.
 END;
 /
